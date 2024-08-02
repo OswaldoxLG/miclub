2e624f3f67c8592cf6d3113c6f237c373988b93a
LyenZo <marcosj.9807@gmail.com>
2024-08-01 20:35:09 -0600

    agrego nosotros

---
+++ paginas/login.php
@@ -0,0 +1,35 @@
+<?php
+include_once '../config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MiClub.site|Inicia Sesión</title>
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/recursos/css/estilo_login.css">
+</head>
+<body class="background">
+    <div class="contenedor_blanco">
+        <div class="logos"> 
+        <img src="<?php echo BASE_URL; ?>/recursos/img/logo.png" class="logo">
+        <img src="<?php echo BASE_URL; ?>/recursos/img/letras.png" class="letras_logo">
+        </div>   
+        <div class="barra_negra"></div>
+        <div>
+            <form method="post" action="#" class="formulario">
+                <h1 class="titulo">BIENVENIDO</h1>
+                <h4 class="texto">Ingresa tu usuario</h4>
+                <input>
+                <h4 class="texto">Contraseña</h4>
+                <input type="password">
+                <br>
+                <br>
+                <button type="submit" class="enviar">INGRESAR</button>
+                <p style="font-family: helvetica;">"Comienza una nueva aventura"</p>
+            </form>
+        </div>
+    </div>
+    <p>algoalgoaglo</p>
+</body>
+</html>
---
+++ paginas/nosotros.php
@@ -0,0 +1,35 @@
+<?php
+include_once '../config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="<?php echo BASE_URL; ?>recursos/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>recursos/css/styles.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>recursos/css/estilo_nosotros.css">
+    <title>Miclub.mx|Sobre nosotros</title>
+    <title>Document</title>
+</head>
+<body>
+<?php
+include_once BASE_PATH . 'includes/header.php';
+?>
+<div class="container">
+    <h1>Conoce más acerca de nosotros</h1>
+    <p>"Conetics" es una empresa dedicada a brindar el servicio de creación,
+diseño, desarrollo y mantenimiento de páginas web. Estamos seriamente comprometidos
+con nuestros clientes y nuestros propios proyectos para brindar un servicio de calidad
+y de esta manera hacer que las proyectos logren tener un buen alcance en el mercado, 
+manteniéndose a la vanguardia con tecnologías actualizadas implementadas para la creación 
+de sitios web.</p>
+</div>
+<div class="logos">
+<img src="<?php echo BASE_URL; ?>recursos/img/conetics.png" class="conetics">
+</div>
+<?php
+include_once BASE_PATH . 'includes/footer.php';
+?>
+</body>
+</html>
