3612054bcf8fd6efbf508a15d238afd42dd07cd9
LyenZo <marcosj.9807@gmail.com>
2024-08-07 06:23:16 -0600

    a

--- includes/header_catalogos.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet"  href="<?php echo BASE_URL; ?>recursos/css/bootstrap.min.css">
-<link rel="stylesheet" href="<?php echo BASE_URL; ?>recursos/css/styles.css">
-
-<div class="header_catalogos">
-    <img src="./recursos/img/logo.png">
-</div>
