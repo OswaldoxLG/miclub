1a82d273ede62249071105fe305b4d1ea5c3ee20
OswaldoxLG <al222310325@gmail.com>
2024-08-10 18:38:06 -0600

    cursos4

--- paginas/cursos_culturales.php
+++ paginas/cursos_culturales.php
@@ -17,53 +17,53 @@ include_once BASE_PATH . 'includes/busqueda.php';
 ?>
 
 <div class="centrar_cursos">
-    <div class="con_titu_cursos"><h1 class="txt_cursos">CURSOS DEPORTIVOS</h1></div>
-    <div class="contenido_cursos_c">
-        <div class="imagenes_cursos">
-            <img src="../recursos/img/curso_futbol.png">
-        </div>
-        <div class="texto_cursos">
-            <h3>TITULO DE EJEMPLO</h3>
-            <p>Descripción de curso</p>
-            <p>costo</p>
-            <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
-        </div>
-    </div>
-    <div class="contenido_cursos_c">
-        <div class="imagenes_cursos">
-            <img src="../recursos/img/slider1.jpg">
-        </div>
-        <div class="texto_cursos">
-            <h3>TITULO DE EJEMPLO</h3>
-            <p>Descripción de curso</p>
-            <p>costo</p>
-            <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
-        </div>
-    </div>
-    <div class="contenido_cursos_c">
-        <div class="imagenes_cursos">
-            <img src="../recursos/img/slider2.jpg">
-        </div>
-        <div class="texto_cursos">
-            <h3>TITULO DE EJEMPLO</h3>
-            <p>Descripción de curso</p>
-            <p>costo</p>
-            <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
-        </div>
-    </div>
-    <div class="contenido_cursos_c">
-        <div class="imagenes_cursos">
-            <img src="#">
-        </div>
-        <div class="texto_cursos">
-            <h3>TITULO DE EJEMPLO</h3>
-            <p>Descripción de curso</p>
-            <p>costo</p>
-            <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
-        </div>
-    </div>
 
-    </div>
+        <div class="con_titu_cursos"><h1 class="txt_cursos">CURSOS DEPORTIVOS</h1></div>
+        <div class="contenido_cursos_c">
+            <div class="imagenes_cursos">
+                <img src="../recursos/img/curso_futbol.png">
+            </div>
+            <div class="texto_cursos">
+                <h3>TITULO DE EJEMPLO</h3>
+                <p>Descripción de curso</p>
+                <p>costo</p>
+                <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
+            </div>
+        </div>
+        <div class="contenido_cursos_c">
+            <div class="imagenes_cursos">
+                <img src="../recursos/img/slider1.jpg">
+            </div>
+            <div class="texto_cursos">
+                <h3>TITULO DE EJEMPLO</h3>
+                <p>Descripción de curso</p>
+                <p>costo</p>
+                <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
+            </div>
+        </div>
+        <div class="contenido_cursos_c">
+            <div class="imagenes_cursos">
+                <img src="../recursos/img/slider2.jpg">
+            </div>
+            <div class="texto_cursos">
+                <h3>TITULO DE EJEMPLO</h3>
+                <p>Descripción de curso</p>
+                <p>costo</p>
+                <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
+            </div>
+        </div>
+        <div class="contenido_cursos_c">
+            <div class="imagenes_cursos">
+                <img src="#">
+            </div>
+            <div class="texto_cursos">
+                <h3>TITULO DE EJEMPLO</h3>
+                <p>Descripción de curso</p>
+                <p>costo</p>
+                <button class="boton_cursos"><a href="#" class="accion_cursos">INSCRIBIRSE</a></button>
+            </div>
+        </div>
+
 </div>
 <?php
 include_once BASE_PATH . 'includes/footer.php';
