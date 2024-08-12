2bd59acac5b589bae5feb2403b3e2241f7c0202d
OswaldoxLG <al222310325@gmail.com>
2024-08-11 19:36:26 -0600

    panel1

---
+++ admin/panel.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="<?php echo BASE_URL; ?>recursos/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>recursos/css/styles.css">
+    <title>Miclub | Panel del administrador</title>
+</head>
+<body>
+    <div class="container con_prin_pan">
+        <div class="tit_cat_pan">
+            <h1 class="txt_cat_pan">CATÁLOGOS</h1>
+        </div>
+    <div class="con_sec_pan">
+        <div class="con_cat_pan">
+            <div class="con_img_pan">
+                <img src="<?php echo BASE_URL; ?>recursos/img/admin.png" alt="imagen administrador" class="img_cat_pan">
+            </div>
+            <div class="con_info_cat">
+                <a href="<?php echo BASE_URL; ?>paginas/categorias.php"><strong class="nom_cat_pan">ADMINISTRADOR</strong></a>
+            </div>
+        </div>
+        <div class="con_cat_pan">
+            <div class="con_img_pan">
+                <img src="<?php echo BASE_URL; ?>recursos/img/instructor.png" alt="imagen instructor" class="img_cat_pan">
+            </div>
+            <div class="con_info_cat">
+                <a href="<?php echo BASE_URL; ?>paginas/categorias.php"><strong class="nom_cat_pan">INSTRUCTOR</strong></a>
+            </div>
+        </div>
+        <div class="con_cat_pan">
+            <div class="con_img_pan">
+                <img src="<?php echo BASE_URL; ?>recursos/img/alumno.png" alt="imagen alumno" class="img_cat_pan">
+            </div>
+            <div class="con_info_cat">
+                <a href="<?php echo BASE_URL; ?>paginas/categorias.php"><strong class="nom_cat_pan">ALUMNOS</strong></a>
+            </div>
+        </div>
+        <div class="con_cat_pan">
+            <div class="con_img_pan">
+                <img src="<?php echo BASE_URL; ?>recursos/img/categorias.png" alt="imagen categorias" class="img_cat_pan">
+            </div>
+            <div class="con_info_cat">
+                <a href="<?php echo BASE_URL; ?>paginas/categorias.php"><strong class="nom_cat_pan">CATEGORÍAS</strong></a>
+            </div>
+        </div>
+        <div class="con_cat_pan">
+            <div class="con_img_pan">
+                <img src="<?php echo BASE_URL; ?>recursos/img/clubes.png" alt="imagen cursos" class="img_cat_pan">
+            </div>
+            <div class="con_info_cat">
+                <a href="<?php echo BASE_URL; ?>paginas/categorias.php"><strong class="nom_cat_pan">CURSOS</strong></a>
+            </div>
+        </div>
+    </div>
+    </div>
+    
+</body>
+</html>
